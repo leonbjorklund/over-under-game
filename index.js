714309f684e77aae8865d00305011d97d8477dd2
Leon Björklund <leon.bjorklund@gmail.com>
2023-02-08 13:22:36 +0100

    minor changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var populationOption = document.getElementById("left-country");
 var leftCountry = document.getElementById("left-country");
 var leftValue = document.getElementById("left-value");
 var rightCountry = document.getElementById("right-country");
-var rightPop = document.getElementById("right-population");
+var rightValue = document.getElementById("right-value");
 var result = document.getElementById("result");
 var streak = document.getElementById("streak");
 var countriesList;
@@ -37,17 +37,20 @@ function chooseCategory() {
     }
     else if (selectedOption == "emissions") {
         chosenProperty = "co2PerCapita2020";
-        while (!countriesList[leftIndex].hasOwnProperty('co2PerCapita2020')) {
-            leftIndex = getRandomInt(countriesList.length);
-        }
-        while (!countriesList[rightIndex].hasOwnProperty('co2PerCapita2020')) {
-            rightIndex = getRandomInt(countriesList.length);
-        }
     }
+    currentStreak = 0;
+    streak.textContent = "Streak: ".concat(currentStreak.toLocaleString());
     placeOptions(chosenProperty);
 }
 function placeOptions(chosen) {
+    while ((!countriesList[leftIndex].hasOwnProperty('co2PerCapita2020') || leftIndex == rightIndex)) {
+        leftIndex = getRandomInt(countriesList.length);
+    }
+    while (!countriesList[rightIndex].hasOwnProperty('co2PerCapita2020')) {
+        rightIndex = getRandomInt(countriesList.length);
+    }
     result.textContent = "";
+    rightValue.textContent = "";
     leftCountry.textContent = countriesList[leftIndex].country;
     leftValue.textContent = countriesList[leftIndex][chosen].toLocaleString("en-US");
     rightCountry.textContent = countriesList[rightIndex].country;
@@ -57,14 +60,34 @@ function compareAnswers(button) {
     var valLeft = countriesList[leftIndex][chosenProperty];
     var valRight = countriesList[rightIndex][chosenProperty];
     if (button.id === "more" && valLeft < valRight || button.id === "less" && valLeft > valRight) {
-        result.textContent = "Correct! ".concat(countryRight, ": ").concat(valRight.toLocaleString("en-US"));
+        result.textContent = "Correct!";
+        switch (chosenProperty) {
+            case "population":
+                rightValue.textContent = "".concat(countryRight, " population: ").concat(valRight.toLocaleString("en-US"));
+                break;
+            case "expectancy":
+                rightValue.textContent = "".concat(countryRight, " life-expectancy: ").concat(valRight.toLocaleString("en-US"), " years");
+                break;
+            case "co2PerCapita2020":
+                rightValue.textContent = "".concat(countryRight, " CO2 emissions per capita: ").concat(valRight.toLocaleString("en-US"), " tons");
+        }
         leftIndex = rightIndex;
         rightIndex = getRandomInt(countriesList.length);
         currentStreak++;
         streak.textContent = "Streak: ".concat(currentStreak.toLocaleString());
     }
     else {
-        result.textContent = "Wrong! ".concat(countryRight, ": ").concat(valRight.toLocaleString("en-US"));
+        result.textContent = "Wrong!";
+        switch (chosenProperty) {
+            case "population":
+                rightValue.textContent = "".concat(countryRight, " population: ").concat(valRight.toLocaleString("en-US"));
+                break;
+            case "expectancy":
+                rightValue.textContent = "".concat(countryRight, " life-expectancy: ").concat(valRight.toLocaleString("en-US"), " years");
+                break;
+            case "co2PerCapita2020":
+                rightValue.textContent = "".concat(countryRight, " CO2 emissions per capita: ").concat(valRight.toLocaleString("en-US"), " tons");
+        }
         currentStreak = 0;
         streak.textContent = "Streak: ".concat(currentStreak.toLocaleString());
         leftIndex = getRandomInt(countriesList.length);
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const leftCountry = document.getElementById("left-country") as HTMLDivElement;
 const leftValue = document.getElementById("left-value") as HTMLDivElement;
 
 const rightCountry = document.getElementById("right-country") as HTMLDivElement;
-const rightPop = document.getElementById("right-population") as HTMLDivElement;
+const rightValue = document.getElementById("right-value") as HTMLDivElement;
 
 const result = document.getElementById("result") as HTMLDivElement;
 const streak = document.getElementById("streak") as HTMLDivElement;
@@ -44,19 +44,24 @@ fetch("./countriesdata.json")
       chosenProperty = "expectancy"
     } else if (selectedOption == "emissions") {
       chosenProperty = "co2PerCapita2020"
-        while (!countriesList[leftIndex].hasOwnProperty('co2PerCapita2020')) {
-          leftIndex = getRandomInt(countriesList.length);
-        }
-        while (!countriesList[rightIndex].hasOwnProperty('co2PerCapita2020')) {
-          rightIndex = getRandomInt(countriesList.length);
-        }
     }
+    currentStreak = 0;
+    streak.textContent = `Streak: ${currentStreak.toLocaleString()}`;
     placeOptions(chosenProperty);
   }
   
 
 function placeOptions(chosen:string) {
+
+  while ((!countriesList[leftIndex].hasOwnProperty('co2PerCapita2020') || leftIndex == rightIndex)) {
+    leftIndex = getRandomInt(countriesList.length);
+  }
+  while (!countriesList[rightIndex].hasOwnProperty('co2PerCapita2020')) {
+    rightIndex = getRandomInt(countriesList.length);
+  }
+
   result.textContent = "";
+  rightValue.textContent = "";
   leftCountry.textContent = countriesList[leftIndex].country;
   leftValue.textContent = countriesList[leftIndex][chosen].toLocaleString("en-US");
   rightCountry.textContent = countriesList[rightIndex].country;
@@ -69,13 +74,33 @@ function compareAnswers(button:HTMLButtonElement) {
   let valRight: number = countriesList[rightIndex][chosenProperty];
 
   if (button.id === "more" && valLeft < valRight || button.id === "less" && valLeft > valRight) {
-    result.textContent = `Correct! ${countryRight}: ${valRight.toLocaleString("en-US")}`
+    result.textContent = "Correct!";
+    switch (chosenProperty) {
+      case "population":
+        rightValue.textContent = `${countryRight} population: ${valRight.toLocaleString("en-US")}`;
+        break;
+      case "expectancy":
+        rightValue.textContent = `${countryRight} life-expectancy: ${valRight.toLocaleString("en-US")} years`;
+        break;
+      case "co2PerCapita2020":
+        rightValue.textContent = `${countryRight} CO2 emissions per capita: ${valRight.toLocaleString("en-US")} tons`;
+    }
     leftIndex = rightIndex;
     rightIndex = getRandomInt(countriesList.length);
     currentStreak++;
     streak.textContent = `Streak: ${currentStreak.toLocaleString()}`;
   } else {
-    result.textContent = `Wrong! ${countryRight}: ${valRight.toLocaleString("en-US")}`
+    result.textContent = "Wrong!";
+    switch (chosenProperty) {
+      case "population":
+        rightValue.textContent = `${countryRight} population: ${valRight.toLocaleString("en-US")}`;
+        break;
+      case "expectancy":
+        rightValue.textContent = `${countryRight} life-expectancy: ${valRight.toLocaleString("en-US")} years`;
+        break;
+      case "co2PerCapita2020":
+        rightValue.textContent = `${countryRight} CO2 emissions per capita: ${valRight.toLocaleString("en-US")} tons`;
+      }
     currentStreak = 0;
     streak.textContent = `Streak: ${currentStreak.toLocaleString()}`;
     leftIndex = getRandomInt(countriesList.length);
@@ -88,7 +113,6 @@ function compareAnswers(button:HTMLButtonElement) {
   while (!countriesList[rightIndex].hasOwnProperty('co2PerCapita2020')) {
     rightIndex = getRandomInt(countriesList.length);
   }
-
 
   setTimeout(() => {
     placeOptions(chosenProperty);
